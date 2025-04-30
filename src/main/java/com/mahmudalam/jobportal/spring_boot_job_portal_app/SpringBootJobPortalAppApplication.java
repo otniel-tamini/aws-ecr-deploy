dd80499807b84764db0da5ba9d6144b22b983720
Mahmud-Alam <official.mahmud23@gmail.com>
2025-04-30 22:54:44 +0600

    starting spring boot project

---
+++ src/main/java/com/mahmudalam/jobportal/spring_boot_job_portal_app/SpringBootJobPortalAppApplication.java
@@ -0,0 +1,13 @@
+package com.mahmudalam.jobportal.spring_boot_job_portal_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJobPortalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJobPortalAppApplication.class, args);
+	}
+
+}

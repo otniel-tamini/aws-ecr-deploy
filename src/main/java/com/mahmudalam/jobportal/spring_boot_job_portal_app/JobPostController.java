f02c6dd6694d1a424366538b3dea5b6f559b749b
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-01 00:07:51 +0600

    create new class for controller

---
+++ src/main/java/com/mahmudalam/jobportal/spring_boot_job_portal_app/JobPostController.java
@@ -0,0 +1,7 @@
+package com.mahmudalam.jobportal.spring_boot_job_portal_app;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobPostController {
+}

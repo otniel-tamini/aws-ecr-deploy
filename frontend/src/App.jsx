541652d333d34a683e090a36b67cf2b83c1b0762
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-06 20:35:32 +0600

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import CreateJob from "./pages/CreatePost";
+import Feed from "./pages/Feed";
 
+function App() {
   return (
-    <></>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employer/dashboard" element={<Dashboard />} />
+        <Route path="employer/create-job" element={<CreateJob />} />
+
+        <Route path="/employee/feed" element={<Feed />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

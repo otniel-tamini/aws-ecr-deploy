3d8774200ee6125318d90bce61635b0bbf101004
Mahmud-Alam <official.mahmud23@gmail.com>
2025-05-11 00:33:40 +0600

    apply useLocation

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,18 @@
-const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
-}
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { useLocation } from "react-router-dom";
 
-export default Layout
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const bg = location.pathname === "/" ? "bg-peach" : "bg-gray-100";
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className={`flex-grow ${bg}`} >{children}</main>
+      <Footer bg={bg} />
+    </div>
+  );
+};
+
+export default Layout;
